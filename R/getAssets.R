06c767c6d732341ea5a2f0e9b546bf79eb412074
shill1729 <52792611+shill1729@users.noreply.github.com>
2022-04-16 17:13:55 -0400

    add timescale

--- R/getAssets.R
+++ R/getAssets.R
@@ -39,3 +39,68 @@ getAssets <- function(symbols, period = "daily", interval = NULL, datatype = "js
     stop("Must past a non-zero list of symbols")
   }
 }
+
+
+#' Get the correct timescale factor for periodic data
+#'
+#' @param period period: "intraday", "daily", "weekly", "monthly"
+#' @param interval NULL for anything but intraday otherwise "1min", "5min", "15min", "30min", "60min"
+#' @param crypto boolean for cryptomarkets (24hrs)
+#'
+#' @description {Return the correct timescale for daily,weekly,monthly, etc.}
+#' @return numeric
+#' @export timescale
+timescale <- function(period = "daily", interval = NULL, crypto = FALSE)
+{
+  intraday_length <- 7.5
+  num_weekdays <- 5
+  num_monthdays<- 21
+  year_length <- 252
+  if(crypto)
+  {
+    intraday_length <- 24
+    num_weekdays <- 7
+    year_length <- 365
+  }
+
+  else if(!crypto)
+  {
+    intraday_length <- 7.5
+    num_weekdays <- 5
+    num_monthdays <- 30
+    year_length <- 252
+  }
+
+  # For intraday, w=e convert to daily figures
+  if(period == "intraday")
+  {
+    if( interval == "1min")
+    {
+      time_scale = 1/(intraday_length*60)
+    } else if(interval == "5min")
+    {
+      time_scale = 1/(intraday_length*12)
+    } else if(interval == "15min")
+    {
+      time_scale = 1/(intraday_length*4)
+    } else if(interval == "30min")
+    {
+      time_scale = 1/(intraday_length*2)
+    } else if(interval == "60min")
+    {
+      time_scale = 1/intraday_length
+    }
+
+
+  } else if(period == "daily")
+  {  # For daily/weekly, we convert to annual figures
+    time_scale = 1/year_length # Crypto trades year-round.
+  } else if(period == "weekly")
+  {
+    time_scale = 1/(year_length/num_weekdays)
+  } else if(period == "monthly")
+  {
+    time_scale <- 1/(year_length/num_monthdays)
+  }
+  return(time_scale)
+}
